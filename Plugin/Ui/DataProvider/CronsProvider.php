67ddbdb7aa2d309dbd2000f20f8a49d40d060c81
Zeeshan Khuwaja <zeeshan_hyder2010@hotmail.com>
2020-10-26 13:15:58 +0800

    [OMNI-5257] item modifiers grid and cron processing

--- Plugin/Ui/DataProvider/CronsProvider.php
+++ Plugin/Ui/DataProvider/CronsProvider.php
@@ -10,14 +10,13 @@ class CronsProvider
 {
 
     /**
-     * This is being used in Hospitality module, so do not change the structure of it.
      * @return mixed
      */
     public function afterReadCronFile(\Ls\Replication\Ui\DataProvider\CronsProvider $subject, $result)
     {
         try {
-            $filePath        = $subject->moduleDirReader->getModuleDir('etc', 'Ls_Hospitality') . '/crontab.xml';
-            $parsedArray     = $subject->parser->load($filePath)->xmlToArray();
+            $filePath          = $subject->moduleDirReader->getModuleDir('etc', 'Ls_Hospitality') . '/crontab.xml';
+            $parsedArray       = $subject->parser->load($filePath)->xmlToArray();
             $hospitalityJobs[] = $parsedArray['config']['_value']['group'];
             // merge both data.
             return array_merge($hospitalityJobs, $result);
