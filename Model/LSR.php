39f6e9bdd02a60624edb4c0154a4adae2375e7b8
Umar Yousaf <umarusaf902@gmail.com>
2021-01-13 12:43:34 +0800

    [OMNI-5350] Adding new cron ProcessItemDeal for supporting deals plus some refactoring

--- Model/LSR.php
+++ Model/LSR.php
@@ -15,6 +15,21 @@ class LSR extends \Ls\Core\Model\LSR
     const SERVICE_MODE_ENABLED = 'ls_mag/service_mode/status';
     const SERVICE_MODE_OPTIONS = 'ls_mag/service_mode/options';
 
+    //For Item Modifiers in Hospitality
+    const SC_SUCCESS_CRON_ITEM_MODIFIER = 'ls_mag/replication/success_process_item_modifier';
+    const SC_ITEM_MODIFIER_CONFIG_PATH_LAST_EXECUTE = 'ls_mag/replication/last_execute_process_item_modifier';
+
+    //For Item Recipes in Hospitality
+    const SC_SUCCESS_CRON_ITEM_RECIPE = 'ls_mag/replication/success_process_item_recipe';
+    const SC_ITEM_RECIPE_CONFIG_PATH_LAST_EXECUTE = 'ls_mag/replication/last_execute_process_item_recipe';
+
+    //For Item Deals in Hospitality
+    const SC_SUCCESS_CRON_ITEM_DEAL = 'ls_mag/replication/success_process_item_deal';
+    const SC_ITEM_DEAL_CONFIG_PATH_LAST_EXECUTE = 'ls_mag/replication/last_execute_process_item_deal';
+
+    const SC_REPLICATION_ITEM_MODIFIER_BATCH_SIZE = 'ls_mag/replication/item_modifier_batch_size';
+    const SC_REPLICATION_ITEM_RECIPE_BATCH_SIZE = 'ls_mag/replication/item_recipe_batch_size';
+
     /**
      * @return mixed
      * @throws NoSuchEntityException
@@ -39,5 +54,19 @@ class LSR extends \Ls\Core\Model\LSR
             ScopeInterface::SCOPE_WEBSITES,
             $this->storeManager->getStore()->getWebsiteId()
         );
+    }
+
+    /**
+     * @param null $storeId
+     * @return bool
+     * @throws NoSuchEntityException
+     */
+    public function isHospitalityStore($storeId = null)
+    {
+        //If StoreID is not passed they retrieve it from the global area.
+        if ($storeId === null) {
+            $storeId = $this->getCurrentStoreId();
+        }
+        return ($this->getCurrentIndustry($storeId) == self::LS_INDUSTRY_VALUE_HOSPITALITY);
     }
 }
