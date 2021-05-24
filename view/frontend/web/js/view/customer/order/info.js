106531a67e16dc5534bd32bd2c52be377dc7e8ed
Syed Waqas Nabi <syedna@lsretail.com>
2021-05-24 19:34:14 +0800

    [OMNI-5441] Remove extra things

--- view/frontend/web/js/view/customer/order/info.js
+++ view/frontend/web/js/view/customer/order/info.js
@@ -1,27 +1,22 @@
 define([
     "jquery",
-    "jquery/ui",
-    "OwlCarousel"
 ], function ($) {
     "use strict";
-    return function main(config, element)
+    return function main(config)
     {
-        var $element = $(element);
         var ajaxUrl = config.ajaxUrl;
         var orderId = config.orderId;
         var storeId = config.storeId;
         $(document).ready(function () {
-            setTimeout(function () {
-                $.ajax({
-                    context: '#ls-hosp-order-info',
-                    url: ajaxUrl,
-                    type: "POST",
-                    data: {orderId: orderId, storeId: storeId}
-                }).done(function (data) {
-                    $('#ls-hosp-order-info').html(data.output).find('.hosp-info-container').trigger('contentUpdated');
-                    return true;
-                });
-            }, 2000);
+            $.ajax({
+                context: '#ls-hosp-order-info',
+                url: ajaxUrl,
+                type: "GET",
+                data: {orderId: orderId, storeId: storeId}
+            }).done(function (data) {
+                $('#ls-hosp-order-info').html(data.output).find('.hosp-info-container').trigger('contentUpdated');
+                return true;
+            });
         });
     };
 });
