0b6604dc0435d83b1f5da2506fce7aff384befbd
Umar Yousaf <umaryo@lsretail.com>
2022-04-22 04:51:18 +0800

    [OMNI-5601] Fixed saving swatch images issues from admin

---
+++ view/base/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Ui/js/dynamic-rows/dynamic-rows': {
+                'Ls_Hospitality/js/dynamic-rows/dynamic-rows-mixin': true
+            }
+        }
+    }
+};
