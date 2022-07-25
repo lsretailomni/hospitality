81d16682a6576feb1f4fb5f26249772a141966bb
Deepak K <deepakkna@lsretail.com>
2022-07-25 12:00:11 +0800

    22856 To fetch ls_modifier_recipe_id

---
+++ Model/Resolver/Product/LsReceipeId.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace Ls\Hospitality\Model\Resolver\Product;
+
+use \Ls\Hospitality\Helper\HospitalityHelper;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\UrlInterface;
+
+class LsReceipeId implements ResolverInterface
+{
+    /**
+     * @var HospitalityHelper
+     */
+    public $hospitalityHelper;
+
+    /**
+     * @param HospitalityHelper $hospitalityHelper
+     */
+    public function __construct(
+        HospitalityHelper $hospitalityHelper
+    ) {
+        $this->hospitalityHelper = $hospitalityHelper;
+    }
+
+    /** Fetch Ls Modefier Receipe Id
+     *
+     * @param Field $field
+     * @param ContextInterface $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     *
+     * @return string
+     *
+     * @throws NoSuchEntityException
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        if (empty($value['ls_modifier_recipe_id'])) {
+            return '';
+        }
+
+        return $value['ls_modifier_recipe_id'];
+    }
+}
