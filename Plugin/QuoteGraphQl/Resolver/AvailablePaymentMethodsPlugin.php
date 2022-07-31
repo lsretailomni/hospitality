ab4267686ea4c1b4c61238080cc1c3e5784dde7b
Deepak K <deepakkna@lsretail.com>
2022-07-31 18:09:37 +0800

    22856 updating available payment methods for click and collect shipping logic in graphql

---
+++ Plugin/QuoteGraphQl/Resolver/AvailablePaymentMethodsPlugin.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Ls\Hospitality\Plugin\QuoteGraphQl\Resolver;
+
+use \Ls\Hospitality\Model\LSR;
+use \Ls\OmniGraphQl\Helper\DataHelper;
+use \Ls\Replication\Model\ResourceModel\ReplStore\Collection;
+use Magento\Checkout\Api\PaymentInformationManagementInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Quote\Api\Data\CartInterface;
+
+/**
+ * AvailablePaymentMethods plugin responsible for filtering payment methods based on click and collect configuration
+ */
+class AvailablePaymentMethodsPlugin
+{
+    /**
+     * @var PaymentInformationManagementInterface
+     */
+    private $informationManagement;
+
+    /**
+     * @param PaymentInformationManagementInterface $informationManagement
+     */
+    public function __construct(
+        PaymentInformationManagementInterface $informationManagement,
+        LSR $lsr
+    ) {
+        $this->informationManagement = $informationManagement;
+        $this->lsr                   = $lsr;
+    }
+
+    /**
+     * Around plugin to filter click and collect stores based on configuration for takeaway
+     *
+     * @param AvailablePaymentMethods $subject
+     * @param Collection $result
+     * @param string $scopeId
+     * @return array
+     * @throws NoSuchEntityException
+     */
+    public function aroundGetPaymentMethodsData(
+        AvailablePaymentMethods $subject,
+        $result,
+        CartInterface $cart
+    ) {
+        $paymentInformation                 = $this->informationManagement->getPaymentInformation($cart->getId());
+        $paymentMethods                     = $paymentInformation->getPaymentMethods();
+        $clickAndCollectPaymentMethodsArr   = [];
+        $clickAndCollectActive              = $this->lsr->getStoreConfig(LSR::SC_CLICKCOLLECT_ACTIVE);
+        $clickAndCollectPaymentMethods      = $this->lsr->getStoreConfig(LSR::SC_CLICKCOLLECT_PAYMENT_OPTION);
+        $shippingMethod                     = $cart->getShippingAddress()->getShippingMethod();
+
+        if ($shippingMethod == "clickandcollect_clickandcollect" &&
+            $clickAndCollectActive &&
+            $clickAndCollectPaymentMethods
+        ) {
+            $clickAndCollectPaymentMethodsArr = explode(",", $clickAndCollectPaymentMethods);
+        }
+
+        $paymentMethodsData = [];
+        foreach ($paymentMethods as $paymentMethod) {
+            if ($clickAndCollectActive && $shippingMethod == "clickandcollect_clickandcollect") {
+                if (in_array($paymentMethod->getCode(), $clickAndCollectPaymentMethodsArr)) {
+                    $paymentMethodsData[] = [
+                        'title' => $paymentMethod->getTitle(),
+                        'code' => $paymentMethod->getCode(),
+                    ];
+                }
+            } else {
+                $paymentMethodsData[] = [
+                    'title' => $paymentMethod->getTitle(),
+                    'code' => $paymentMethod->getCode(),
+                ];
+            }
+        }
+
+        return $paymentMethodsData;
+    }
+}
