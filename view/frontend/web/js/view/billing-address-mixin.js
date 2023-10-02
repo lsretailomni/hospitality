344c36d281f98f962e4ecbcca6c8fc3abf5ed924
Syed Waqas Nabi <syedna@lsretail.com>
2023-10-02 20:24:02 +0800

    [#42162] Fixed issue with billing address at payment page

--- view/frontend/web/js/view/billing-address-mixin.js
+++ view/frontend/web/js/view/billing-address-mixin.js
@@ -4,10 +4,10 @@ define(['Magento_Checkout/js/model/quote'], function (quote) {
     return function (Component) {
         return Component.extend({
             defaults: {
-                template:(!window.checkoutConfig.hasOwnProperty("anonymous_order") &&
-                    !window.checkoutConfig.anonymous_order.is_enabled) ||
-                (!window.checkoutConfig.hasOwnProperty("remove_checkout_step_enabled") &&
-                    !window.checkoutConfig.remove_checkout_step_enabled) ? 'Magento_Checkout/billing-address' : 'Ls_Hospitality/checkout/billing-address'
+                template: (window.checkoutConfig.hasOwnProperty("anonymous_order") &&
+                    window.checkoutConfig.anonymous_order.is_enabled) ||
+                (window.checkoutConfig.hasOwnProperty("remove_checkout_step_enabled") &&
+                    window.checkoutConfig.remove_checkout_step_enabled) ? 'Ls_Hospitality/checkout/billing-address' : 'Magento_Checkout/billing-address'
             }
         });
     }
