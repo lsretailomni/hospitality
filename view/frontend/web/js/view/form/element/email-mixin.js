34db5d42b97741d1fcae48cbc74a1c01aecfe4f9
Umar Yousaf <umar.yousaf@briteskies.com>
2023-10-04 15:58:16 +0800

    [#43342] Fixed email template override in case of remove checkout step is enabled

--- view/frontend/web/js/view/form/element/email-mixin.js
+++ view/frontend/web/js/view/form/element/email-mixin.js
@@ -5,9 +5,9 @@ define([], function () {
         return Component.extend({
             defaults: {
                 template: (window.checkoutConfig.hasOwnProperty("anonymous_order") &&
-                    !window.checkoutConfig.anonymous_order.is_enabled) ||
+                    window.checkoutConfig.anonymous_order.is_enabled) ||
                 (window.checkoutConfig.hasOwnProperty("remove_checkout_step_enabled") &&
-                    !window.checkoutConfig.remove_checkout_step_enabled) ? 'Magento_Checkout/form/element/email' : 'Ls_Hospitality/checkout/view/form/element/email',
+                    window.checkoutConfig.remove_checkout_step_enabled) ? 'Ls_Hospitality/checkout/view/form/element/email': 'Magento_Checkout/form/element/email',
             },
 
             showLoginForm: function () {
