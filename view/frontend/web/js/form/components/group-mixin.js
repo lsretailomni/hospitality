be3fc20ab34bcfaad1c9ebd7812aa0b93ad9d357
Syed Waqas Nabi <syedna@lsretail.com>
2024-12-05 20:54:45 +0800

    [#60904] Fixed issue with date validation while placing order and fixed issue with disable place order button

--- view/frontend/web/js/form/components/group-mixin.js
+++ view/frontend/web/js/form/components/group-mixin.js
@@ -10,7 +10,7 @@ define([], function () {
                     window.checkoutConfig.anonymous_order.is_enabled &&
                     !window.checkoutConfig.anonymous_order.required_fields.hasOwnProperty("street")
                 ) {
-                    this.setVisible(false);
+                    //this.setVisible(true);
                     return this;
                 }
                 return this;
