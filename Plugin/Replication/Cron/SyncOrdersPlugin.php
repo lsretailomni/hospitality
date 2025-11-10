fc7a26227d008c9717d3ef8c083cf8912a0e91f8
Waqas <syedna@lsretail.com>
2025-11-10 13:02:47 +0800

    [#72846] Showing order Id in hospitality

---
+++ Plugin/Replication/Cron/SyncOrdersPlugin.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Ls\Hospitality\Plugin\Replication\Cron;
+
+use \Ls\Hospitality\Helper\HospitalityHelper;
+use \Ls\Replication\Cron\SyncOrders;
+
+/**
+ * Plugin for SyncOrders cron
+ */
+class SyncOrdersPlugin
+{
+    /**
+     * @var HospitalityHelper
+     */
+    private $hospitalityHelper;
+
+    /**
+     * @param HospitalityHelper $hospitalityHelper
+     */
+    public function __construct(
+        HospitalityHelper $hospitalityHelper
+    ) {
+        $this->hospitalityHelper = $hospitalityHelper;
+    }
+
+    /**
+     * After plugin for execute method
+     *
+     * @param SyncOrders $subject
+     * @param array $result
+     * @return array
+     */
+    public function afterExecute(SyncOrders $subject, $result)
+    {
+        // Process orders that were just synced
+        $orders = $this->hospitalityHelper->getOrdersWithDocumentIdWithoutLsOrderId($subject->store->getId());
+
+        if (!empty($orders)) {
+            foreach ($orders as $order) {
+                $this->hospitalityHelper->saveHospOrderId($order, false);
+            }
+        }
+        return $result;
+    }
+}
