50962c7b31de358b935890605da7546a339ed4fb
Deepak K <deepakkna@lsretail.com>
2026-04-17 13:21:27 +0800

    79842 Tips suggestions

--- view/frontend/web/js/view/tips.js
+++ view/frontend/web/js/view/tips.js
@@ -33,31 +33,56 @@ define([
             this.isLoading = tipsLoader.isLoading;
             this.isLoaded = tipsLoader.isLoaded;
             this.error = tipsLoader.error;
-
             this.selected = tipsState.selected;
             this.customValue = tipsState.customValue;
+
+            // show/hide based on payment step visibility
+            this.isPaymentStep = ko.observable(false);
+            var self = this;
+            function updateStepVisibility() {
+                var paymentStep = stepNavigator.steps().find(function (step) {
+                    return step.code === 'payment';
+                });
+
+                self.isPaymentStep(
+                    !!(paymentStep && typeof paymentStep.isVisible === 'function' && paymentStep.isVisible())
+                );
+            }
+
+            // initial state
+            updateStepVisibility();
+
+            stepNavigator.steps.subscribe(function () {
+                updateStepVisibility();
+            });
+
+            // react when payment step visibility changes
+            var paymentStep = stepNavigator.steps().find(function (step) {
+                return step.code === 'payment';
+            });
+
+            if (paymentStep && typeof paymentStep.isVisible === 'function') {
+                paymentStep.isVisible.subscribe(function () {
+                    updateStepVisibility();
+                });
+            }
 
             if (!this.isLoaded()) {
                 tipsLoader.load();
             }
-
             this.selectedLabel = ko.computed(function () {
                 var s = this.selected();
-
                 if (s === 'other') {
                     return this.customValue() || 'Other';
                 }
-
                 var found = this.tipOptions().find(function (o) {
                     return o.value === s;
                 });
-
                 return found ? found.label : '';
             }, this);
 
             this.selected.subscribe(function (val) {
                 var amount = 0;
-
                 if (val === 'other') {
                     amount = parseFloat(tipsState.customValue()) || 0;
                 } else {
@@ -66,7 +91,6 @@ define([
                     var base = parseFloat(totals.base_grand_total || totals.grand_total || 0);
                     amount = Math.round(((percentage / 100) * base) * 100) / 100;
                 }
-
                 saveTip(amount);
             });
 
@@ -78,11 +102,9 @@ define([
 
             function saveTip(amount) {
                 var postData = { tip: amount };
-
                 if (typeof window.FORM_KEY !== 'undefined') {
                     postData.form_key = window.FORM_KEY;
                 }
-
                 $.ajax({
                     url: '/ls_hospitality/ajax/savetip',
                     method: 'POST',
